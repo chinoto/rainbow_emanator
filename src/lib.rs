89c1da1906e9dcb5bcf7b63ca39e0d259d07df4b
Damian J Pound <damian@thebestisp.com>
2021-11-13 12:54:30 -0600

    Simplify x&y scaling

--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,15 @@ pub fn rainbow_emanator(width: u32, height: u32, ms: f32) -> impl Iterator<Item 
     if width == 0 || height == 0 {
         panic!("Do not use 0 for width or height.");
     }
-    let x_factor = (width - 1) as f32 / 2.0;
-    let y_factor = (height - 1) as f32 / 2.0;
     // Multiplication is faster than division.
-    let x_div = 1.0 / x_factor;
-    let y_div = 1.0 / y_factor;
+    let x_div = 2.0 / (width - 1).max(1) as f32;
+    let y_div = 2.0 / (height - 1).max(1) as f32;
 
     (0..height).flat_map(move |y| {
         (0..width).map(move |x| {
-            // Scale extremes to -1..1 and avoid division by 0...
-            let x: f32 = if x_factor == 0.0 {
-                0.0
-            } else {
-                (x as f32 - x_factor) * x_div
-            };
-
-            let y: f32 = if y_factor == 0.0 {
-                0.0
-            } else {
-                (y as f32 - y_factor) * y_div
-            };
+            // Scale extremes to -1..1
+            let x: f32 = x as f32 * x_div - 1.;
+            let y: f32 = y as f32 * y_div - 1.;
 
             let dist: f32 = palette::float::Float::sqrt(x * x + y * y);
             const TIME_DIV: f32 = 1.0 / 3000.0;
