c36763bc68c561a28b54b9fea4273e6e2091bc45
Damian J Pound <damian@thebestisp.com>
2021-11-13 12:42:01 -0600

    Get x&y from iteration; remove strength_reduce

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 use palette::{convert::FromColorUnclamped, encoding::srgb::Srgb, rgb::Rgb, Hsv};
-use strength_reduce::StrengthReducedU32;
 
 /// Provide the width and height of your LED board. If it is a strip, just set
 /// either one to 1, whichever makes more sense to you. Using 0 will panic.
@@ -15,29 +14,30 @@ pub fn rainbow_emanator(width: u32, height: u32, ms: f32) -> impl Iterator<Item 
     // Multiplication is faster than division.
     let x_div = 1.0 / x_factor;
     let y_div = 1.0 / y_factor;
-    let width_div = StrengthReducedU32::new(width);
 
-    (0..width * height).map(move |i| {
-        // Scale extremes to -1..1 and avoid division by 0...
-        let x: f32 = if x_factor == 0.0 {
-            0.0
-        } else {
-            ((i % width_div) as f32 - x_factor) * x_div
-        };
+    (0..height).flat_map(move |y| {
+        (0..width).map(move |x| {
+            // Scale extremes to -1..1 and avoid division by 0...
+            let x: f32 = if x_factor == 0.0 {
+                0.0
+            } else {
+                (x as f32 - x_factor) * x_div
+            };
 
-        let y: f32 = if y_factor == 0.0 {
-            0.0
-        } else {
-            ((i / width_div) as f32 - y_factor) * y_div
-        };
+            let y: f32 = if y_factor == 0.0 {
+                0.0
+            } else {
+                (y as f32 - y_factor) * y_div
+            };
 
-        let dist: f32 = palette::float::Float::sqrt(x * x + y * y);
-        const TIME_DIV: f32 = 1.0 / 3000.0;
-        const VALUE: f32 = 2.0 / 256.0;
-        let hsv = Hsv::new((-ms * TIME_DIV + dist * 0.6) * 360.0, 1.0, VALUE);
-        let Rgb {
-            red, green, blue, ..
-        } = Rgb::<Srgb>::from_color_unclamped(hsv).into_format::<u8>();
-        [red, green, blue]
+            let dist: f32 = palette::float::Float::sqrt(x * x + y * y);
+            const TIME_DIV: f32 = 1.0 / 3000.0;
+            const VALUE: f32 = 2.0 / 256.0;
+            let hsv = Hsv::new((-ms * TIME_DIV + dist * 0.6) * 360.0, 1.0, VALUE);
+            let Rgb {
+                red, green, blue, ..
+            } = Rgb::<Srgb>::from_color_unclamped(hsv).into_format::<u8>();
+            [red, green, blue]
+        })
     })
 }
